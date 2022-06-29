cf2e5af57b4f9430d7dce01f11df0a6b6aaf14fa
Alamin934 <islamalamin934@gmail.com>
2022-06-29 13:12:28 +0600

    get appointments data

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -1,11 +1,21 @@
 import { Typography } from '@mui/material';
 import React from 'react';
+import { useState, useEffect } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const Appointments = () => {
+    const { user } = useAuth();
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/appointments?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setAppointments(data))
+    }, [user.email])
     return (
         <div>
             <Typography variant="h6" noWrap component="div">
-                Appointments
+                Appointments {appointments.length}
             </Typography>
         </div>
     );
