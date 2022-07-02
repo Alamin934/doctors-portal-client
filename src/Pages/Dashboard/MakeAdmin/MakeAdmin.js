6a6f06f91cc68ecd5c6174beea9576f57e38e868
Alamin934 <islamalamin934@gmail.com>
2022-07-02 16:43:33 +0600

    create admin role api to send email for make admin

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import { Alert, Button, TextField } from '@mui/material';
 import { Box } from '@mui/system';
+import { useState } from 'react';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const handleOnBlur = e => {
+        setEmail(e.target.value);
+    }
+    const handleSubmit = e => {
+        const user = { email }
+        fetch('http://localhost:5000/users/admin', {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    setSuccess(true)
+                }
+            })
+        e.preventDefault();
+    }
 
     return (
         <div>
             <h2>Make Admin</h2>
             <Box style={{ width: "400px", margin: '0px auto' }}>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <TextField
                         label="Email"
                         sx={{ width: '100%', mb: 2 }}
                         id="outlined-size-small"
                         size="small"
                         name="email"
+                        onBlur={handleOnBlur}
                     />
                     <Button type="submit" variant="contained">Make Admin</Button>
                 </form>
-                <Alert sx={{ mt: 3 }} severity="success">Made Admin Successfully</Alert>
+                {success && <Alert sx={{ mt: 3 }} severity="success">Made Admin Successfully</Alert>}
             </Box>
         </div>
     );
